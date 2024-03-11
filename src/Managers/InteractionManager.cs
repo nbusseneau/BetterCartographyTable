nbusseneau/BetterCartographyTable

commit 96c89867f168790b81f445769d5a994de7346dbb

restrict interactions to sharable pins

src/Managers/InteractionManager.cs
@@ -123,8 +123,8 @@ public static class InteractionManager
       return;
     }
 
-    // all other actions require interacting with a map table
-    if (!IsInteracting) return;
+    // all other actions require interacting with a sharable pin on a map table
+    if (!IsInteracting && !closestPin.IsSharable) return;
     var isTablePin = closestPin.IsMapTablePin(CurrentMapTable);
 
     var modifierPlusLeftClickingPrivatePin = Plugin.IsModifierKeyPushed && closestPin.IsPrivate;
@@ -167,8 +167,8 @@ public static class InteractionManager
       return;
     }
 
-    // all other actions require interacting with a map table
-    if (!IsInteracting) return;
+    // all other actions require interacting with a sharable pin on a map table
+    if (!IsInteracting && !closestPin.IsSharable) return;
     var isTablePin = closestPin.IsMapTablePin(CurrentMapTable);
 
     var modifierPlusRightClickingPrivatePin = Plugin.IsModifierKeyPushed && closestPin.IsPrivate;

src/Model/SharablePinData.cs
@@ -13,7 +13,7 @@ namespace BetterCartographyTable.Model;
 /// </summary>
 public class SharablePinData : PinData, IEquatable<PinData>
 {
-  private static readonly HashSet<PinType> interactablePinTypes = [
+  private static readonly HashSet<PinType> sharablePinTypes = [
     PinType.Icon0,
     PinType.Icon1,
     PinType.Icon2,
@@ -26,7 +26,7 @@ public class SharablePinData : PinData, IEquatable<PinData>
   ];
 
   public SharingMode SharingMode { get; set; } = SharingMode.Private;
-  public bool IsInteractable => interactablePinTypes.Contains(this.m_type);
+  public bool IsSharable => sharablePinTypes.Contains(this.m_type);
   public bool IsPrivate => this.SharingMode == SharingMode.Private;
   public bool IsPublic => this.SharingMode == SharingMode.Public;
   public bool IsGuild => this.SharingMode == SharingMode.Guild;

commit 0f64af189f9b1f6648383e9324478134ac3d02ee

fix shift-click without interacting with table
Fixes: 96c89867f168790b81f445769d5a994de7346dbb

src/Managers/InteractionManager.cs
@@ -26,7 +26,6 @@ public static class InteractionManager
   public static void OnMapTableUse(MapTable mapTable, Humanoid user)
   {
     if (IsInteracting) return;
-
     var hasAccess = HasAccess(mapTable);
     if (!hasAccess) user.Message(MessageHud.MessageType.Center, Localization.instance.Localize("$piece_noaccess"), 0, null);
     else if (Plugin.IsModifierKeyPushed && GuildsManager.IsEnabled) TryToggleMode(mapTable, user);
@@ -124,7 +123,7 @@ public static class InteractionManager
     }
 
     // all other actions require interacting with a sharable pin on a map table
-    if (!IsInteracting && !closestPin.IsSharable) return;
+    if (!IsInteracting || !closestPin.IsSharable) return;
     var isTablePin = closestPin.IsMapTablePin(CurrentMapTable);
 
     var modifierPlusLeftClickingPrivatePin = Plugin.IsModifierKeyPushed && closestPin.IsPrivate;
@@ -168,7 +167,7 @@ public static class InteractionManager
     }
 
     // all other actions require interacting with a sharable pin on a map table
-    if (!IsInteracting && !closestPin.IsSharable) return;
+    if (!IsInteracting || !closestPin.IsSharable) return;
     var isTablePin = closestPin.IsMapTablePin(CurrentMapTable);
 
     var modifierPlusRightClickingPrivatePin = Plugin.IsModifierKeyPushed && closestPin.IsPrivate;