402b91611cf6a6e71d8e6a4c3fcee77f8defcbf8
Nicolas Busseneau <nicolas@busseneau.fr>
2024-05-25 04:15:33 +0200

    streamline release workflow

--- publish.ps1
+++ publish.ps1
@@ -4,10 +4,21 @@ param(
   [Parameter(Mandatory = $true)][string]$thunderstoreToken
 )
 
+$ErrorActionPreference = "Stop"
+function StopOnError { if (-not $?) { exit } }
+
+# Make sure repo is up to date
+git pull; StopOnError
+git push; StopOnError
+
+# Make sure package can build
+dotnet build -c Release; StopOnError
+tcli build; StopOnError
+
+# Publish GitHub release
 $owner = "nbusseneau"
 $repo = "BetterCartographyTable"
 $workflow = "release.yaml"
-$packageName = "Better_Cartography_Table"
 
 $headers = @{
   Authorization = [String]::Format("Bearer {0}", $githubToken)
@@ -21,7 +32,7 @@ $body = @{
   }
 } | ConvertTo-Json
 Write-Output "Sending request to workflow_dispatch | Tag: $tag"
-$response = Invoke-RestMethod -Method POST -Uri $url -Headers $headers -Body $body
+Invoke-RestMethod -Method POST -Uri $url -Headers $headers -Body $body
 
 $attempt = 0
 do {
@@ -39,12 +50,15 @@ if ($release.tag_name -ne $tag) {
   Write-Error -Message "Latest release not matching input tag, something went wrong with workflow_dispatch" -ErrorAction Stop
 }
 
-git pull
-dotnet build -c Release
-tcli publish --token $thunderstoreToken
-$assetName = "$owner-$packageName-$tag.zip"
-$assetPath = "build/$assetName"
+# Update repo following version rotation via release
+git pull; StopOnError
 
+# Publish package
+dotnet build -c Release; StopOnError
+tcli publish --token $thunderstoreToken; StopOnError
+
+# Upload asset to Github release
+$assetPath = (Get-ChildItem -Path "build/" -Filter "*$tag.zip").FullName
 $url = "https://uploads.github.com/repos/$owner/$repo/releases/$($release.id)/assets?name=$assetName"
 Write-Output "Uploading package to release | Asset name: $assetName"
-$response = Invoke-RestMethod -Method POST -ContentType "application/zip" -Uri $url -Headers $headers -InFile $assetPath
+Invoke-RestMethod -Method POST -ContentType "application/zip" -Uri $url -Headers $headers -InFile $assetPath
