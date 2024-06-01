39719c365ae34f7c8c519f2f313ca45b0dba0dc9
Nicolas Busseneau <nicolas@busseneau.fr>
2024-06-01 19:16:08 +0200

    improve release process

--- publish.ps1
+++ publish.ps1
@@ -7,63 +7,75 @@ param(
 $ErrorActionPreference = "Stop"
 function StopOnError { if (-not $?) { exit } }
 
-# Make sure repo is up to date
-git pull; StopOnError
-git push; StopOnError
-
-# Make sure package can build
-dotnet build -c Release; StopOnError
-tcli build; StopOnError
-
-# Publish GitHub release
-$owner = "nbusseneau"
-$repo = "BetterCartographyTable"
+$repo = (Select-String -Path "thunderstore.toml" -Pattern 'websiteUrl = "https://github.com/(.*)"').Matches.Groups[1]
 $workflow = "release.yaml"
 
 $headers = @{
   Authorization = [String]::Format("Bearer {0}", $githubToken)
 }
 
-$url = "https://api.github.com/repos/$owner/$repo/releases/latest"
+$url = "https://api.github.com/repos/$repo/releases/latest"
 $release = Invoke-RestMethod -Uri $url -Headers $headers
+
+# Default case: no existing release for this tag
 if ($release.tag_name -ne $tag) {
-  Write-Output "Latest release $($release.tag_name) not matching input tag $tag"
-  $url = "https://api.github.com/repos/$owner/$repo/actions/workflows/$workflow/dispatches"
+  Write-Output "Latest release $($release.tag_name) not matching input tag $tag, try to make new release"
+
+  # Make sure repo is up to date
+  git pull; StopOnError
+  git push; StopOnError
+
+  # Make sure package can build
+  dotnet build -c Release; StopOnError
+  tcli build; StopOnError
+
+  # Trigger release workflow
+  $url = "https://api.github.com/repos/$repo/actions/workflows/$workflow/dispatches"
   $body = @{
     ref    = "main"
     inputs = @{
       tag = $tag
     }
   } | ConvertTo-Json
-  Write-Output "Sending request to workflow_dispatch | Tag: $tag"
+  Write-Output "Sending workflow_dispatch request to $workflow workflow | Tag: $tag"
   Invoke-RestMethod -Method POST -Uri $url -Headers $headers -Body $body
 
   $attempt = 0
   do {
-    $url = "https://api.github.com/repos/$owner/$repo/releases/latest"
+    $url = "https://api.github.com/repos/$repo/releases/latest"
     $release = Invoke-RestMethod -Uri $url -Headers $headers
     $attempt++
-    Write-Output "Fetched latest release: $($release.tag_name) | Attempt: $attempt"
     if ($release.tag_name -ne $tag -and $attempt -ne 10) {
       Start-Sleep -Seconds 10
     }
   }
   until ($release.tag_name -eq $tag -or $attempt -eq 10)
   if ($release.tag_name -ne $tag) {
-    Write-Error -Message "Latest release $($release.tag_name) still not matching input tag $tag, something went wrong with workflow_dispatch" -ErrorAction Stop
+    Write-Error -Message "Latest release $($release.tag_name) still not matching input tag $tag, something went wrong with $workflow workflow" -ErrorAction Stop
+  }
+
+  # Update repo following version rotation via release
+  git pull; StopOnError
+}
+
+# Special case: existing release for this tag, i.e. we already ran this script before but it failed after creating the release
+else {
+  Write-Output "Latest release $($release.tag_name) matching input tag $tag, check if repo state is in sync with existing release"
+
+  # Make sure repo is synced with release state
+  $currentHeadTag = git describe --exact-match HEAD; StopOnError
+  if ($release.tag_name -ne $currentHeadTag) {
+    Write-Error -Message "Latest release $($release.tag_name) not matching current HEAD tag $currentHeadTag, discrepancy between local repo and existing release state" -ErrorAction Stop
   }
 }
-Write-Output -Message "Latest release $($release.tag_name) matching input tag $tag, start publishing process"
 
-# Update repo following version rotation via release
-git pull; StopOnError
-
-# Publish package
+# Build and publish package to Thunderstore
+Write-Output -Message "Start publishing process"
 dotnet build -c Release; StopOnError
 tcli publish --token $thunderstoreToken; StopOnError
 
-# Upload asset to Github release
-$asset = (Get-ChildItem -Path "build/" -Filter "*$tag.zip")
-$url = "https://uploads.github.com/repos/$owner/$repo/releases/$($release.id)/assets?name=$($asset.Name)"
-Write-Output "Uploading package to release | Asset name: $($asset.Name)"
-Invoke-RestMethod -Method POST -ContentType "application/zip" -Uri $url -Headers $headers -InFile $asset.FullName
+# Upload package to Github release
+$package = (Get-ChildItem -Path "build/" -Filter "*$tag.zip")
+$url = "https://uploads.github.com/repos/$repo/releases/$($release.id)/assets?name=$($package.Name)"
+Write-Output "Uploading package to release | Asset name: $($package.Name)"
+Invoke-RestMethod -Method POST -ContentType "application/zip" -Uri $url -Headers $headers -InFile $package.FullName
